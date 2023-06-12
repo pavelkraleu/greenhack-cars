67db038403f1e62de3c60e18d62201a00277671f
Pavel Kral <pavel@pavelkral.eu>
2023-06-12 19:54:50 +0200

    Add help text to car form

--- cars/web/forms.py
+++ cars/web/forms.py
@@ -7,3 +7,7 @@ class CarDriveForm(forms.ModelForm):
     class Meta:
         model = CarDrive
         fields = ["proposed_type", "frozen"]
+        help_texts = {
+            "proposed_type": "You can hardcode some car for this license plate.",
+            "frozen": "This car will be ignored in analysis and proposed type will not be changed.",
+        }
